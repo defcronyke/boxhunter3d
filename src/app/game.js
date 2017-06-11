55f145b05433299f625cec3e9d469922fbe16680
Jeremy Carter <jeremy@jccss.ca>
2017-06-11 16:27:15 -0400

    a triangle with feet!?

--- src/app/game.js
+++ src/app/game.js
@@ -10,7 +10,7 @@ export default class Game {
 
     window.addEventListener('keydown', e => {
       // console.log('keyCode: ', e.keyCode);
-      this.speed = 0.5;
+      this.speed = 0.7;
       this.speedX = 0;
       this.speedY = 0;
       this.speedZ = 0;
@@ -158,23 +158,95 @@ export default class Game {
 
   addTriangularPrism() {
     const diameter = 16;
-    const height = 20;
+    const height = 10;
+    const mass = 1;
+    const footDiameter = 1;
     const prism = BABYLON.MeshBuilder.CreateCylinder('triangularPrism1', {
       diameter,
       height,
       tessellation: 3
     }, this.scene);
     prism.checkCollisions = true;
-    // prism.position.y = 5;
+    prism.position.y = 12;
     prism.rotate(BABYLON.Axis.X, (90 * Math.PI) / 180, BABYLON.Space.WORLD);
     prism.rotate(BABYLON.Axis.Z, (90 * Math.PI) / 180, BABYLON.Space.WORLD);
+
+    const foot1 = BABYLON.Mesh.CreateSphere('foot1', 16, footDiameter, this.scene);
+    // foot1.position.x = (-diameter / 2) + (foot1Diameter / 2);
+    // foot1.position.y = 12;
+    foot1.position.x = (-diameter / 2) + (footDiameter * 4.5);
+    foot1.position.y = (-height / 2) + (footDiameter);
+    foot1.position.z = (-diameter / 2) + (footDiameter * 2);
+    foot1.physicsImpostor = new BABYLON.PhysicsImpostor(
+      foot1,
+      BABYLON.PhysicsImpostor.SphereImpostor,
+      {mass, restitution: 0.4, friction: 1},
+      this.scene
+    );
+    foot1.parent = prism;
+
+    // const foot2Diameter = 3;
+    const foot2 = BABYLON.Mesh.CreateSphere('foot2', 16, footDiameter, this.scene);
+    // foot2.position.x = (diameter / 2) - (foot2Diameter / 2);
+    // foot2.position.y = 12;
+    foot2.position.x = (-diameter / 2) + (footDiameter * 4.5);
+    foot2.position.y = (-height / 2) + (footDiameter);
+    foot2.position.z = (diameter / 2) - (footDiameter * 2);
+    foot2.physicsImpostor = new BABYLON.PhysicsImpostor(
+      foot2,
+      BABYLON.PhysicsImpostor.SphereImpostor,
+      {mass, restitution: 0.4, friction: 1},
+      this.scene
+    );
+    foot2.parent = prism;
+
+    // const foot3Diameter = 3;
+    const foot3 = BABYLON.Mesh.CreateSphere('foot3', 16, footDiameter, this.scene);
+    // foot3.position.x = (-diameter / 2) + (foot3Diameter / 2);
+    // foot3.position.z = 12;
+    foot3.position.x = (-diameter / 2) + (footDiameter * 4.5);
+    foot3.position.y = (height / 2) - (footDiameter);
+    foot3.position.z = (-diameter / 2) + (footDiameter * 2);
+    foot3.physicsImpostor = new BABYLON.PhysicsImpostor(
+      foot3,
+      BABYLON.PhysicsImpostor.SphereImpostor,
+      {mass, restitution: 0.4, friction: 1},
+      this.scene
+    );
+    foot3.parent = prism;
+
+    // const foot4Diameter = 3;
+    const foot4 = BABYLON.Mesh.CreateSphere('foot4', 16, footDiameter, this.scene);
+    // foot4.position.x = (diameter / 2) - (foot4Diameter / 2);
+    // foot4.position.z = 12;
+    foot4.position.x = (-diameter / 2) + (footDiameter * 4.5);
+    foot4.position.y = (height / 2) - (footDiameter);
+    foot4.position.z = (diameter / 2) - (footDiameter * 2);
+    foot4.physicsImpostor = new BABYLON.PhysicsImpostor(
+      foot4,
+      BABYLON.PhysicsImpostor.SphereImpostor,
+      {mass, restitution: 0.4, friction: 1},
+      this.scene
+    );
+    foot4.parent = prism;
 
     prism.impostor1 = new BABYLON.PhysicsImpostor(
       prism,
       BABYLON.PhysicsImpostor.MeshImpostor,
-      {mass: 0, restitution: 0.4, friction: 1},
+      {mass, restitution: 0.4, friction: 1},
       this.scene
     );
+
+    const box = BABYLON.MeshBuilder.CreateBox('box1', {width: 5, height: 5}, this.scene);
+    box.position.x = 10;
+    box.position.y = 5;
+    box.impostor1 = new BABYLON.PhysicsImpostor(
+      box,
+      BABYLON.PhysicsImpostor.BoxImpostor,
+      {mass: 1, restitution: 0.4, friction: 1},
+      this.scene
+    );
+
     // const prism = BABYLON.MeshBuilder.CreatePolyhedron('triangularPrism1', {type: 5, size: 3}, this.scene);
     // prism.position.y = 5;
     // console.log('rotation: ', prism.rotation);
@@ -193,7 +265,7 @@ export default class Game {
   level1() {
     const groundMat1 = new BABYLON.StandardMaterial('groundMat1', this.scene);
     groundMat1.diffuseTexture = new BABYLON.Texture('assets/img/gridPattern1.jpg', this.scene);
-    this.ground = BABYLON.Mesh.CreateGroundFromHeightMap('ground1', 'assets/img/gridPattern1.jpg', 100, 100, 250, 0, 1, this.scene, false, () => {
+    this.ground = BABYLON.Mesh.CreateGroundFromHeightMap('ground1', 'assets/img/gridPattern1.jpg', 100, 100, 250, 0, 0.5, this.scene, false, () => {
       this.ground.material = groundMat1;
       this.ground.physicsImpostor = new BABYLON.PhysicsImpostor(
         this.ground,
